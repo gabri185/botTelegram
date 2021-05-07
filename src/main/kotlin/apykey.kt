b64dfce4c6758fab1e1399e6d0e899b501579bd0
gabri <gdominguezreyes@danielcastelao.org>
2021-05-07 09:41:59 +0200

    Todo listo para empezar a hacer cositas con el bot

---
+++ src/main/kotlin/apykey.kt
@@ -0,0 +1,3 @@
+object Apykey{
+    val miToken= "1824464553:AAEdn7mLgzky405ix1fy5CM94Cd_9Eqr2xw"
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,7 @@
+import com.github.kotlintelegrambot.bot
+
+fun main(args: Array<String>) {
+    val bot = bot{
+        token = Apykey.miToken
+    }
+}
